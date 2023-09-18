cdfc19d63349e8d2980f1ff565f37d54b860f4ee
Omer Rahmanovic <omer.6g@gmail.com>
2023-09-18 10:18:33 +0200

    show map view, update user location

---
+++ Betshops/Betshops/View/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  Betshops
+//
+//  Created by Omer Rahmanovic on 18. 9. 2023..
+//
+
+import SwiftUI
+import MapKit
+import Combine
+
+struct MapView: View {
+    @StateObject private var viewModel = MapViewModel()
+    
+    var body: some View {
+        GeometryReader { proxy in
+            Map(coordinateRegion: $viewModel.region, showsUserLocation: true, userTrackingMode: .none)
+                .ignoresSafeArea(.all)
+        }
+    }
+}
