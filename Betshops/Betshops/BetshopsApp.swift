f41b967a90fc3a21f58b9bac043f2718884228a2
Omer Rahmanovic <omer.6g@gmail.com>
2023-09-18 09:41:06 +0200

    add project files

---
+++ Betshops/Betshops/BetshopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetshopsApp.swift
+//  Betshops
+//
+//  Created by Omer Rahmanovic on 18. 9. 2023..
+//
+
+import SwiftUI
+
+@main
+struct BetshopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
